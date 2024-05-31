ee79cf1c80fba43be0951547ba709e55e5a82d28
Muslehud77 <fardinmohit@gmail.com>
2024-05-31 22:35:39 +0800

    fixed the bug Regarding the id genaration

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -1,22 +1,30 @@
 import { TAcademicSemester } from "../academicSemester/academicSemester.interface";
-import { Student } from "../student/student.model";
-import { StudentModel } from './../student/student.interface';
+
 import { userModel } from "./user.model";
 
 // for making id we will need year semesterCode 4 digit number
 
-const lastStudentId = async ()=>{
+const findLastStudentId = async ()=>{
     const lastStudent = await userModel
       .findOne({ role: 'student' }, { id: 1, _id: 0 })
       .sort({ createdAt: -1 })
       .lean();
-    return lastStudent?.id ? lastStudent?.id.slice(-4) : undefined;
+    return lastStudent?.id ? lastStudent?.id : undefined;
 }
 
 export const generateStudentId = async (payload: TAcademicSemester) => {
+    let currentId = '0'
+    const lastStudentId = await findLastStudentId();
+    const lastStudentSemesterYear = lastStudentId?.substring(0,4)
+    const lastStudentSemesterCode = lastStudentId?.substring(4,6)
+    const currentSemesterCode = payload.code
+    const currentSemesterYear = payload.year
+
+    //if lastSemester year and code matches with the current year and code it will reassign the value of currentId with lastStudent id otherwise the the id counting will start again 
+    if(lastStudentId&& lastStudentSemesterCode=== currentSemesterCode && lastStudentSemesterYear === currentSemesterYear){
+        currentId = lastStudentId.substring(4)
+    }
    
-    const currentId = await lastStudentId() || '0'
-    // getting the last student id
    
       let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
       incrementId = `${payload.year}${payload.code}${incrementId}`;
